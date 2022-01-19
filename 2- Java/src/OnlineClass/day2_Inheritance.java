4b463d843b50fa900173d32661e5191df82d6690
shashank <shashank.shukla1947@gmail.com>
2022-01-19 23:37:20 +0530

    Updated Java day2

--- 2- Java/src/OnlineClass/day2_Inheritance.java
+++ 2- Java/src/OnlineClass/day2_Inheritance.java
@@ -7,45 +7,80 @@ public class day2_Inheritance {
 }
 
 // Final class cannot be inherited
-/*
- * public final class day2_Inheritance { int salary; }
- */
+
+public final class day2_Inheritance {
+	int salary;
+}
 
 //---------------------------------------
 //Single Inheritance
-/*
- * class subClass extends day2_Inheritance{ int bonus=10000;
- * 
- * public static void main(String[] args) { subClass i= new subClass();
- * 
- * System.out.println("Salary is "+i.salary);
- * System.out.println("Bonus is "+i.bonus); } }
- */
+
+class subClass extends day2_Inheritance {
+	int bonus = 10000;
+
+	public static void main(String[] args) {
+		subClass i = new subClass();
+
+		System.out.println("Salary is " + i.salary);
+		System.out.println("Bonus is " + i.bonus);
+	}
+}
 
 //--------------------------------------------
 // Multi level Inheritance
-/*
- * class subClass1 extends day2_Inheritance{ void fun2() {
- * System.out.println("fun 2"); } }
- * 
- * class subClass extends subClass1{ void fun3() { System.out.println("fun 3");
- * } public static void main(String[] args) { subClass c1=new subClass();
- * c1.fun1(); c1.fun2(); c1.fun3(); } }
- */
+
+class subClass1 extends day2_Inheritance {
+	void fun2() {
+		System.out.println("fun 2");
+	}
+}
+
+class subClass extends subClass1 {
+	void fun3() {
+		System.out.println("fun 3");
+	}
+
+	public static void main(String[] args) {
+		subClass c1 = new subClass();
+		c1.fun1();
+		c1.fun2();
+		c1.fun3();
+	}
+}
 
 //-------------------------------------------------
 // Method Overriding
-/*
- * class Animal{ public void sound() {
- * System.out.println("Animal makes different sounds"); } } class Dog extends
- * Animal{ public void sound() { super.sound(); System.out.println("Dog barks");
- * } } class Cat extends Animal{ public void sound() {
- * System.out.println("Cat makes sounnd MEoww"); } } class subClass{ public
- * static void main(String[] args) { Animal a1=new Animal(); Dog a2=new Dog();
- * Cat a3=new Cat();
- * 
- * a1.sound(); a2.sound(); a3.sound(); } }
- */
+
+class Animal {
+	public void sound() {
+		System.out.println("Animal makes different sounds");
+	}
+}
+
+class Dog extends Animal {
+	public void sound() {
+		super.sound();
+		System.out.println("Dog barks");
+	}
+}
+
+class Cat extends Animal {
+	public void sound() {
+		System.out.println("Cat makes sounnd MEoww");
+	}
+}
+
+class subClass {
+	public static void main(String[] args) {
+		Animal a1 = new Animal();
+		Dog a2 = new Dog();
+		Cat a3 = new Cat();
+
+		a1.sound();
+		a2.sound();
+		a3.sound();
+	}
+}
 
 //Use of SUPER keryword
 class Employee {
