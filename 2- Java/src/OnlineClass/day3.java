c1a5d9b0e8da52a3f1f65beb0fbbf4b21f8bb3f1
shashank <shashank.shukla1947@gmail.com>
2022-01-20 10:29:50 +0530

    Updated day3

--- 2- Java/src/OnlineClass/day3.java
+++ 2- Java/src/OnlineClass/day3.java
@@ -16,6 +16,7 @@ public class day3 {
 		//OR int arr[] = {10,20,30,40,50,60,70};
 		for(int i=0;i<7;i++)
 			System.out.println(arr[i]);
+		System.out.println("Enhanced for loop");
 		for(int k:arr) { //Enhanced for loop
 			System.out.println(k);
 		}
@@ -28,6 +29,7 @@ public class day3 {
 		day3 d3=new day3();
 		
 		day3 arrObject[] = {d1, d2, d3};
+		System.out.println("Array of Objects");
 		for(int i=0;i<3;i++)
 			System.out.println(arrObject[i]);
 		//-------------------------------------
@@ -42,12 +44,32 @@ public class day3 {
 						{4,5,6},
 						{7,8,9}
 					  };
+		System.out.println("2-D array");
 		for(int i=0;i<3;i++) {
 			for(int j=0;j<3;j++) {
 				System.out.println(arr4[i][j]);
 			}
-		
 		}
+		System.out.println("Enhanced for loop in 2-D array");
+		for(int k[]:arr4) {
+			for(int i:k)
+				System.out.println(i);
+		}
+		//----------------------------------------
+		
+		//JAGGED Array
+		int arr5[][]= {
+				{1,2},
+				{3,4,5,6},
+				{7,8}
+		};
+		System.out.println("Jagged array");
+		for(int i=0;i<arr5.length;i++) {
+			for(int j=0;j<arr5[i].length;j++) {
+				System.out.println(arr5[i][j]);
+			}
+		}
+		
 	}
 
 }
