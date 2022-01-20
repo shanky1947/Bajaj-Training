2fcc72e05aca8732f257e6a72b22f8282468ece5
shashank <shashank.shukla1947@gmail.com>
2022-01-20 12:59:45 +0530

    Added custom wrapper class

--- 2- Java/src/OnlineClass/day3_Wrapper_Class.java
+++ 2- Java/src/OnlineClass/day3_Wrapper_Class.java
@@ -3,7 +3,7 @@ package OnlineClass;
 public class day3_Wrapper_Class {
 
 	//Wrapper class- converts primitive data types into objects, as object has various advantages, can be passed by reference
-	//Autoboxing
+	
 	public static void main(String[] args) {
 		int i = 10; 					//integer primitive data type
 		
@@ -14,6 +14,34 @@ public class day3_Wrapper_Class {
 		int j = i1.intValue();  		//Unboxing -> convert from object to primitive data type
 		
 		int k = i1;						//Auto-unboxing -> automtic converion to pdt
+		
+		System.out.println(((Object)k).getClass().getSimpleName()); //to check class of data type
+		
+		
+		//CUSTOM WC
+		wrapperClass w = new wrapperClass(10);
+		System.out.println(w.toSting());
 	}
 
 }
+
+// CUSTOM WRAPPER CLASS - my class only responsible for converting primitive dt to objects
+class wrapperClass{
+	private int a;
+	
+	wrapperClass(int a){
+		this.a=a;
+	}
+	
+	public int show() {
+		return a;
+	}
+	public void define(int a) {
+		this.a=a;
+	}
+	public String toSting() {  //wrapping up my integer data into string
+		return Integer.toString(a);
+	}
+}
+
+
