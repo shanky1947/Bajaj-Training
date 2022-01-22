d25707a2082a08cdaf9de9a10d63d8c5bd36c6b2
shashank <shashank.shukla1947@gmail.com>
2022-01-22 19:15:15 +0530

    Added JDBC POSTGRESQL file

--- 2- Java/src/OnlineClass/day5_JDBC_PostgreSQL.java
+++ 2- Java/src/OnlineClass/day5_JDBC_PostgreSQL.java
@@ -1,10 +1,34 @@
 package OnlineClass;
+import java.sql.*;
 
 public class day5_JDBC_PostgreSQL {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException{
+		Connection con = connection();
+		insert(con);
+		
+	}
+	public static Connection connection() throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+		try {
+			String username = "postgres";
+			String password = "101298";
+			String database = "bajaj";
+			
+			Connection con = null;
+			Class.forName("org.postgresql.Driver");
+			con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/"+database, username, password);
+			System.out.println("Connection is sucessful");
+			return con;
+		}
+		catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return null;
+	}
+	public static void insert(Connection con) throws SQLException {
+		Statement stmt = con.createStatement();
+		stmt.executeUpdate("insert into employee values (14, 'Pranav', 24, 35000); ");
+		System.out.println("Record insterted succesfully");
 	}
 
 }
