26e31abf040e336eec2e7c75c037558ec9508e70
shashank <shashank.shukla1947@gmail.com>
2022-01-25 12:34:31 +0530

    Updated demo_fileserver file

--- 4- Node JS & Express JS/demo_fileserver.js
+++ 4- Node JS & Express JS/demo_fileserver.js
@@ -9,3 +9,20 @@
 // var qdata = q.query; //returns an object: { year: 2017, month: 'february' }
 // console.log(qdata.month); //returns 'february'
 
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;  //./summer.html
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8081);
