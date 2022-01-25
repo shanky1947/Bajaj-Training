6e5901c1fc8f8a84a2737377e6e314709a1f00ed
shashank <shashank.shukla1947@gmail.com>
2022-01-25 17:39:29 +0530

    Added express folder

--- 4- Node JS & Express JS/index.js
+++ 4- Node JS & Express JS/index.js
@@ -1,7 +1,9 @@
-const http = require('http');
+var express = require('express');
 
-const server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end("Hello World");
-}).listen(8081);
+var app = new express();
 
+app.get('/', function(req, res){
+    res.send("hello world");
+});
+
+app.listen(8081);
