97411ac82e148a08a493ce29705a7054d479959b
shashank <shashank.shukla1947@gmail.com>
2022-01-26 19:09:40 +0530

    Added comments in index.js file

--- 4- Node JS & Express JS/index.js
+++ 4- Node JS & Express JS/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = new express();
 
+// app.get(route, callback)
+// callback function has 2 parameters, request and response
 app.get('/', function(req, res){
     res.send("hello world");
 });
 
+//app.listen(port, [host], [backlog], [callback]])
 app.listen(8081);
