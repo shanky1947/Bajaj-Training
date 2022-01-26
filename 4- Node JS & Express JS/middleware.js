67e805c7ca95bc180d9546b9ee41ba3bc7518567
shashank <shashank.shukla1947@gmail.com>
2022-01-26 21:11:50 +0530

    Added body parser and cookie parser middleware

--- 4- Node JS & Express JS/middleware.js
+++ 4- Node JS & Express JS/middleware.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var app = express();
 
+
+//THIRD PARTY MIDDLEWARE
+//BODY PARSER
+var bodyParser = require('body-parser');
+//To parse URL encoded data
+app.use(bodyParser.urlencoded({ extended: false }))
+//To parse json data
+app.use(bodyParser.json())
+
+//COOKIE PARSER
+var cookieParser = require('cookie-parser');
+app.use(cookieParser())
+
+
+
 //First middleware before response is sent
 app.use(function(req, res, next){
     console.log("Start");
@@ -18,4 +33,6 @@ app.use('/', function(req, res){
 });
 
 app.listen(8081);
+
+
 
