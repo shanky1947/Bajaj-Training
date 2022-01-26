599aa3337ee40b97c2c476044d7008c60d9ea9cb
shashank <shashank.shukla1947@gmail.com>
2022-01-26 20:28:26 +0530

    Updated url_building.js file

--- 4- Node JS & Express JS/url_building.js
+++ 4- Node JS & Express JS/url_building.js
@@ -5,4 +5,16 @@ app.get('/:id', function(req, res){
     res.send('The id you is specified is: ' + req.params.id);
 });
 
+app.get('/things/:id([0-9]{5})', function(req, res){
+    res.send('id: '+req.params.id);
+});
+
+app.get('/things/:name/:id', function(req, res){
+    res.send('id: '+req.params.id+' and name: '+req.params.name);
+});
+
+app.get('*', function(req, res){
+    res.send('Sorry, this is an invalid URL');
+});
+
 app.listen(8081);
