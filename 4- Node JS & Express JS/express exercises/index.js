373d8fde2022bd04c48e4264ae9c9f67015c1d2f
shashank <shashank.shukla1947@gmail.com>
2022-01-27 21:01:06 +0530

    Finished till database from tutorials point

--- 4- Node JS & Express JS/express exercises/index.js
+++ 4- Node JS & Express JS/express exercises/index.js
@@ -5,15 +5,14 @@ var app = express();
 // Routing in index.js file
 // app.get(route, callback)
 // callback function has 2 parameters, request and response
-// app.get('/', function(req, res){
-//     res.send("hello world");
-// });
+app.get('/', function(req, res){
+    res.send("hello world");
+});
 
 // Using things.js to separately define routing
 var things = require('./things');
 
 app.use('/things', things);
-
 
 //app.listen(port, [host], [backlog], [callback]])
 app.listen(8081);
