e63efc85509c60793434511824f1b119f51e7cb6
shashank <shashank.shukla1947@gmail.com>
2022-01-28 16:12:02 +0530

    Finished REST API project

--- 4- Node JS & Express JS/REST Project- Movie API/movies.js
+++ 4- Node JS & Express JS/REST Project- Movie API/movies.js
@@ -44,6 +44,54 @@ router.post('/', function(req, res){
     }
 });
 
+router.put('/:id', function(req, res){
+    if(!req.body.name ||
+        !req.body.year.toString().match(/^[0-9]{4}$/g) ||
+        !req.body.rating.toString().match(/^[0-9]\.[0-9]$/g) ||
+        !req.params.id.toString().match(/^[0-9]{3,}$/g)){
+        
+        res.status(400);
+        res.json({message: "Bad Request"});
+     }
+    else{
+        var updateIndex = movies.map(function(movie){
+            return movie.id;
+        }).indexOf(parseInt(req.params.id));
+        if(updateIndex === -1){
+            //Movie not found, create new
+            movies.push({
+               id: req.params.id,
+               name: req.body.name,
+               year: req.body.year,
+               rating: req.body.rating
+            });
+            res.json({message: "New movie created.", location: "/movies/" + req.params.id});
+         } else {
+            //Update existing movie
+            movies[updateIndex] = {
+               id: req.params.id,
+               name: req.body.name,
+               year: req.body.year,
+               rating: req.body.rating
+            };
+            res.json({message: "Movie id " + req.params.id + " updated.", 
+               location: "/movies/" + req.params.id});
+         }
+      }
+});
 
+router.delete('/:id', function(req, res){
+    
+    var removeIndex = movies.map(function(movie){
+       return movie.id;
+    }).indexOf(parseInt(req.params.id)); //Gets us the index of movie with given id.
+    
+    if(removeIndex === -1){
+       res.json({message: "Not found"});
+    } else {
+       movies.splice(removeIndex, 1);
+       res.send({message: "Movie id " + req.params.id + " removed."});
+    }
+ });
 
 module.exports = router;
