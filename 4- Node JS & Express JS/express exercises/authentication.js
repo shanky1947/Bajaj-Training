939a0127aecdc1f6d7bd68ba8d588c11fd83fdc1
shashank <shashank.shukla1947@gmail.com>
2022-01-28 12:51:23 +0530

    Added authentication files

--- 4- Node JS & Express JS/express exercises/authentication.js
+++ 4- Node JS & Express JS/express exercises/authentication.js
@@ -21,4 +21,72 @@ app.get('/signup', function(req, res){
     res.render('signup');
 });
 
+app.post('/signup', function(req, res){
+    if(!req.body.id || !req.body.password){
+        res.status("400");
+        res.send("Invalid details!");
+    }
+    else{
+        Users.filter(function(user){
+            if(user.id === req.body.id){
+                res.render('signup', {message: "User already exist! Login ot choose another user id"});
+            }
+        });
+        var newUser = {id: req.body.id, password: req.body.password};
+        Users.push(newUser);
+        req.session.user = newUser;
+        res.redirect('/protected_page');
+    }
+});
+
+function checkSignIn(req, res, next){
+    if(req.session.user){
+        next(); //If session exists proceed to page
+    }
+    else{
+        var err = new Error("Not logged in!");
+        console.log(req.session.user);
+        next(err);  //Error, trying to acces unauthorized page!
+    }
+};
+
+app.get('/protected_page', checkSignIn, function(req, res){
+    res.render('protected_page', {id:req.session.user.id});
+});
+
+app.use('/protected_page', function(err, req, res, next){
+    console.log(err);
+    //User should be authenticated, Redirect him to login page
+    res.redirect('/login');
+});
+
+app.get('/login', function(req, res){
+    res.render('login');
+});
+
+app.post('/login', function(req, res){
+    console.log(Users);
+    if(!req.body.id || !req.body.password){
+        res.render('login', {message: "Please enter both id and password"});
+    }
+    else{
+        Users.filter(function(user){
+            if(user.id === req.body.id && user.password === req.body.password){
+                req.session.user=user;
+                res.redirect('/protected_page');
+            }
+            else   
+            res.render('login', {message: "Invalid credentials!"});
+        });
+    }
+});
+
+app.get('/logout', function(req, res){
+    req.session.destroy(function(){
+        console.log("user logged out.");
+    });
+    res.redirect('/login');
+})
+
+
 app.listen(3000);
